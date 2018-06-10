6295e0ca2945183816e14a30bc48fc1f8e83402f
lukakralj <luka.kralj2@gmail.com>
2018-06-10 18:51:37 +0100

    Updated Controller to work without the boolean to check if the game is currently playing; the buttons work properly without throwing exceptions.

--- src/gameTetris/Controller.java
+++ src/gameTetris/Controller.java
@@ -3,18 +3,14 @@ package gameTetris;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    private Scene scene;
-    private MyGrid grid;
     private GameManager gameManager;
     private Thread currentThread;
-    private boolean playing;
 
     @FXML private Label score;
     @FXML private Button startButton;
@@ -25,42 +21,40 @@ public class Controller implements Initializable {
     @FXML
     public void initialize(URL arg0, ResourceBundle res) {
         MyGrid gridPane = new MyGrid();
-        scene = Main.getMainScene();
-        grid = gridPane;
+        gameManager = new GameManager(Main.getMainScene(), gridPane);
         mainPane.setCenter(gridPane);
-        playing = false;
     }
 
     @FXML
     private void handleStartButton(ActionEvent event) {
-        if (playing) {
+        if (currentThread != null) {
             gameManager.play();
         }
         else {
-            gameManager = new GameManager(scene, grid);
             currentThread = new Thread(gameManager);
             currentThread.start();
-            playing = true;
         }
 
     }
 
     @FXML
     private void handlePauseButton(ActionEvent event) {
-        gameManager.pause();
+        if (currentThread != null) {
+            gameManager.pause();
+        }
     }
 
     @FXML
     private void handleResetButton(ActionEvent event) {
-        if (playing) {
-            playing = false;
-            gameManager.resetGame();
+        if (currentThread != null) {
+            gameManager.stopGame();
             try {
                 currentThread.join();
             }
             catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            currentThread = null;
         }
         gameManager.resetGame();
     }
